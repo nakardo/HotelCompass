e6e7fefca644bbb3f7f7a3efc0b602cb2d78497b
= <diegonake@gmail.com>
2014-02-12 12:22:55 -0200

    added empty Settings file

---
+++ HotelCompass/Settings.h
@@ -0,0 +1,10 @@
+//
+//  Settings.h
+//  HotelCompass
+//
+//  Created by Diego Acosta on 2/12/14.
+//  Copyright (c) 2014 Diego Acosta. All rights reserved.
+//
+
+static NSString * const kBookingAPIAuthUserId = @"";
+static NSString * const kBookingAPIAuthPassword = @"";
